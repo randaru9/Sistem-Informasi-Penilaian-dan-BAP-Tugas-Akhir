2ae21cd58f7bb064c1dd45b7f06723c5c0c62039
randaru09 <ndaruanggoro123@gmail.com>
2024-05-21 17:19:54 +0700

    style(component): navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <div class="w-full h-20 flex items-center justify-between">
-        <img src="{{ url(asset('storage/assets/logo_dashboard_login.svg')) }}" class="h-fit w-1/2 md:w-1/3" alt="logo_prodi">
+    <div class="w-full h-20 hidden md:flex p-0 items-center justify-between">
+        <img src="{{ url(asset('storage/assets/logo_dashboard_login.svg')) }}" class="h-full w-1/2 md:w-fit hidden lg:flex lg:flex-col" alt="logo_prodi">
         <div class="flex space-x-5 justify-center items-center mr-6">
             <button data-modal-target={{ $modal }} data-modal-toggle={{ $modal }}>
                 <img src="{{ url(asset('storage/assets/logo_notif.svg')) }}" alt="logo_notif">
             </button>
-            <div class="">
+            <div>
                 <p class="font-poppins font-semibold text-base">Rangga</p>
                 <p class="font-poppins font-normal text-xs">Mahasiswa</p>
             </div>
