edff4c4311c9b2b075bc36e385f20da354ac7941
randaru09 <ndaruanggoro123@gmail.com>
2024-05-21 15:44:02 +0700

    feat(component): add navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <div class="w-full h-20 flex items-center justify-between">
+        <img src="{{ url(asset('storage/assets/logo_dashboard_login.svg')) }}" class="h-fit w-1/2 md:w-1/3" alt="logo_prodi">
+        <div class="flex space-x-5 justify-center items-center mr-6">
+            <button data-modal-target={{ $modal }} data-modal-toggle={{ $modal }}>
+                <img src="{{ url(asset('storage/assets/logo_notif.svg')) }}" alt="logo_notif">
+            </button>
+            <div class="">
+                <p class="font-poppins font-semibold text-base">Rangga</p>
+                <p class="font-poppins font-normal text-xs">Mahasiswa</p>
+            </div>
+            <img src="{{ url(asset('storage/assets/logo_profil.svg')) }}" alt="logo_profil">
+        </div>
+    </div>
+</div>
