685fa1b2f2dbccda287114fbfe5aafa4d9dde96c
randaru09 <ndaruanggoro123@gmail.com>
2024-05-21 17:18:30 +0700

    feat(component): add breadcrumbs

---
+++ resources/views/components/breadcrumbs.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <ul class="flex p-3">
+        @foreach ($collection  as $item)
+        @if ($loop->first)
+        <li>
+            <a href="{{ $item['href'] }}" class="font-poppins font-normal">
+                {{ $item['title'] }}
+            </a>
+        </li>
+        @else
+        <li class="flex">
+            <img src="{{ url(asset('storage/assets/logo_arrow_breadcrumbs.svg')) }}" alt="">
+            <a href="{{ $item['href'] }}" class="{{$loop->last ? 'font-poppins font-normal text-gold' : 'font-poppins font-normal'}}">
+                {{ $item['title'] }}
+            </a>
+        </li>
+        @endif
+        @endforeach
+    </ul>
+</div>
