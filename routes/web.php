a486669598083f0954a373a8ea24277686f1ea4a
randaru09 <ndaruanggoro123@gmail.com>
2024-05-23 01:00:52 +0700

    feat(route): add some route

--- routes/web.php
+++ routes/web.php
@@ -23,38 +23,52 @@ Route::get('/login', function () {
     return view('auth.login');
 });
 Route::get('/lupa-katasandi', function () {
-    return view('auth.lupa-katasandi');    
+    return view('auth.lupa-katasandi');
 });
 Route::get('/lengkapi-data-diri', function () {
-    return view('auth.lengkapi-data-diri');    
+    return view('auth.lengkapi-data-diri');
 });
 Route::get('/verifikasi-otp', function () {
-    return view('auth.verifikasi-otp');    
+    return view('auth.verifikasi-otp');
 });
 Route::get('/buat-katasandi-baru', function () {
-    return view('auth.buat-katasandi-baru');    
+    return view('auth.buat-katasandi-baru');
 });
 
 Route::prefix('mahasiswa')->group(function () {
+    Route::prefix('seminar')->group(function () {
+        Route::get('/', function () {
+            return view('mahasiswa.seminar.seminar');
+        })->name('seminar');
 
-    Route::get('/seminar', function () {
-        return view('mahasiswa.seminar');    
-    })->name('seminar');
+        Route::get('/tambah', function () {
+            return view('mahasiswa.seminar.seminar-tambah');
+        })->name('seminar-tambah');
 
-    Route::get('/seminar/tambah', function () {
-        return view('mahasiswa.seminar-tambah');    
-    })->name('seminar-tambah');
+        Route::get('/detail', function () {
+            return view('mahasiswa.seminar.seminar-detail');
+        })->name('seminar-detail');
 
-    Route::get('/seminar/detail', function () {
-        return view('mahasiswa.seminar-detail');    
-    })->name('seminar-detail');
+        Route::get('/ubah', function () {
+            return view('mahasiswa.seminar.seminar-ubah');
+        })->name('seminar-ubah');
 
-    Route::get('/seminar/ubah', function () {
-        return view('mahasiswa.seminar-ubah');    
-    })->name('seminar-ubah');
+        Route::get('/cek-revisi', function () {
+            return view('mahasiswa.seminar.seminar-cek-revisi');
+        })->name('seminar-cek-revisi');
+    });
 
-    Route::get('/seminar/cek-revisi', function () {
-        return view('mahasiswa.seminar-cek-revisi');    
-    })->name('seminar-cek-revisi');
-
+    Route::prefix('yudisium')->group(function () {
+        Route::get('/', function () {
+            return view('mahasiswa.yudisium.yudisium');
+        })->name('yudisium');
+        Route::get('/tambah', function () {
+            return view('mahasiswa.yudisium.yudisium-tambah');
+        })->name('yudisium-tambah');
+        Route::get('/detail', function () {
+            return view('mahasiswa.yudisium.yudisium-detail');
+        })->name('yudisium-detail');
+    });
 });
+
+
