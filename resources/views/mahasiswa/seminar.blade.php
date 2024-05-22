d98c721214a31f5be171415962fe3e90055da9d5
randaru09 <ndaruanggoro123@gmail.com>
2024-05-22 15:40:10 +0700

    style(component): rename file

--- resources/views/mahasiswa/seminar.blade.php
+++ resources/views/mahasiswa/seminar.blade.php
@@ -1,3 +1,45 @@
-<x-layout-mahasiswa title="Seminar">
-    
+@php
+    $breads = [
+        [
+            'href' => '/mahasiswa/seminar',
+            'title' => 'Seminar',
+        ],
+    ];
+
+    $tablehead = ['No', 'Tanggal/Waktu Seminar', 'Status Seminar', 'Status Revisi', 'Aksi'];
+
+    $data = [
+        'array' => [
+            [
+                'Tanggal' => '10-10-2022',
+                'Waktu' => '13.00',
+                'Status Seminar' => 'Seminar Proposal',
+                'Status Revisi' => 'Belum Selesai',
+            ],
+            [
+                'Tanggal' => '10-10-2022',
+                'Waktu' => '13.00',
+                'Status Seminar' => 'Seminar Proposal',
+                'Status Revisi' => 'Belum Selesai',
+            ],
+            [
+                'Tanggal' => '10-10-2022',
+                'Waktu' => '13.00',
+                'Status Seminar' => 'Seminar Proposal',
+                'Status Revisi' => 'Belum Selesai',
+            ],
+        ],
+        'page' => 3,
+        'current' => 1,
+    ];
+@endphp
+
+<x-layout-mahasiswa :$breads title="Seminar">
+    <div class="flex justify-end mb-4">
+        <a href="/mahasiswa/seminar/tambah" class="p-2 font-poppins font-medium text-white bg-gold rounded-[4px]">Buat Jadwal Seminar</a>
+    </div>
+    <x-table-seminar-mahasiswa :$tablehead :$data />
+    <div class="mt-4 flex justify-end">
+        <x-pagination :pages="$data['page']" :current="$data['current']"/>
+    </div>
 </x-layout-mahasiswa>
