7ffa830a26aa5669bbb6ce6474b4513b7bff8509
randaru09 <ndaruanggoro123@gmail.com>
2024-05-22 21:36:22 +0700

    feat(view): remove table component from seminar page

--- resources/views/mahasiswa/seminar.blade.php
+++ resources/views/mahasiswa/seminar.blade.php
@@ -36,10 +36,47 @@
 
 <x-layout-mahasiswa :$breads title="Seminar">
     <div class="flex justify-end mb-4">
-        <a href="/mahasiswa/seminar/tambah" class="p-2 font-poppins font-medium text-white bg-gold rounded-[4px]">Buat Jadwal Seminar</a>
+        <a href="/mahasiswa/seminar/tambah" class="p-2 font-poppins font-medium text-white bg-gold rounded-[4px]">Buat
+            Jadwal Seminar</a>
     </div>
-    <x-table-seminar-mahasiswa :$tablehead :$data />
+    <div>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg ring-2 ring-blue1">
+            <table class="w-full text-sm text-left rtl:text-right">
+                <thead class="text-xs text-blue1 bg-blue2 border-b-2 border-[#BEDFF9]">
+                    <tr>
+                        @foreach ($tablehead as $item)
+                            <th scope="col" class="px-6 py-3 font-semibold font-poppins text-base">
+                                {{ $item }}
+                            </th>
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($data['array'] as $item)
+                        <tr class="bg-white border-b font-poppins text-base font-normal">
+                            <td class="px-6 py-4">
+                                {{ $loop->index + 1 }}
+                            </td>
+                            <td scope="row" class="px-6 py-4 font-poppins text-base font-normal">
+                                {{ $item['Tanggal'] }} / {{ $item['Waktu'] }}
+                            </td>
+                            <td class="px-6 py-4 font-semibold font-poppins text-base">
+                                {{ $item['Status Seminar'] }}
+                            </td>
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                {{ $item['Status Revisi'] }}
+                            </td>
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <a href="/mahasiswa/seminar/detail"
+                                    class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline">Detail</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
     <div class="mt-4 flex justify-end">
-        <x-pagination :pages="$data['page']" :current="$data['current']"/>
+        <x-pagination :pages="$data['page']" :current="$data['current']" />
     </div>
 </x-layout-mahasiswa>
