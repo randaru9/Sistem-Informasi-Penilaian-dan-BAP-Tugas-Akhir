212f94540f46cf668741c44161527e4f6d00aeb6
randaru09 <ndaruanggoro123@gmail.com>
2024-05-22 20:03:24 +0700

    style(component): add some style in navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="w-full h-20 hidden md:flex p-0 items-center justify-between">
+    <div class="w-full h-20 hidden md:flex p-0 items-center justify-between border-b-2">
         <img src="{{ url(asset('storage/assets/logo_dashboard_login.svg')) }}" class="h-full w-1/2 md:w-fit hidden lg:flex lg:flex-col" alt="logo_prodi">
         <div class="flex space-x-5 justify-center items-center mr-6">
             <button data-modal-target={{ $modal }} data-modal-toggle={{ $modal }}>
