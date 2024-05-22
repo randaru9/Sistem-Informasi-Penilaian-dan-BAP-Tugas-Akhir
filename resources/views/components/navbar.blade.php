be87bb983c171740d74b5a06a289e651de0eae9a
randaru09 <ndaruanggoro123@gmail.com>
2024-05-23 00:58:49 +0700

    style(component): fix some typo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
-<div>
     <div class="w-full h-20 hidden md:flex p-0 items-center justify-between border-b-2">
-        <img src="{{ url(asset('storage/assets/logo_dashboard_login.svg')) }}" class="h-full w-1/2 md:w-fit hidden lg:flex lg:flex-col" alt="logo_prodi">
+        <img src="{{ url(asset('storage/assets/logo_dashboard_login.svg')) }}"
+            class="h-full w-1/2 md:w-fit hidden lg:flex lg:flex-col" alt="logo_prodi">
         <div class="flex space-x-5 justify-center items-center mr-6">
             <button data-modal-target={{ $modal }} data-modal-toggle={{ $modal }}>
                 <img src="{{ url(asset('storage/assets/logo_notif.svg')) }}" alt="logo_notif">
@@ -12,4 +12,3 @@
             <img src="{{ url(asset('storage/assets/logo_profil.svg')) }}" alt="logo_profil">
         </div>
     </div>
-</div>
