6c071f3e59aa050ff2e560820528785023982b57
randaru09 <ndaruanggoro123@gmail.com>
2024-05-28 22:05:54 +0700

    feat(view) : add ubah email dosen page

---
+++ resources/views/dosen/profil/profil-ubah-email.blade.php
@@ -0,0 +1,37 @@
+@php
+    $breads = [
+        [
+            'href' => '/dosen/profil',
+            'title' => 'Profil',
+        ],
+        [
+            'href' => '/dosen/profil/ubah-email',
+            'title' => 'Ubah Email',
+        ],
+    ];
+@endphp
+
+<x-layout-dosen :$breads title="Ubah Email">
+    <div class="bg-white ring-2 ring-blue1 rounded-[10px] w-full h-full overflow-y-auto">
+        <form action="/dosen/profil/verifikasi-email">
+            <div class="w-full px-5 flex py-2 gap-2">
+                <div class="w-1/2">
+                    <p for="nama" class="block mb-2 text-xl text-[#000000] font-poppins font-bold">Ubah Email</p>
+                </div>
+            </div>
+            <div class="w-full px-5 flex py-2 gap-2">
+                <div class="w-1/2">
+                    <label for="email"
+                        class="block mb-2 text-sm text-[#000000] font-poppins font-normal">Email</label>
+                    <input type="email" id="email"
+                        class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full "
+                        minlength="9" required />
+                </div>
+            </div>
+            <div class="w-full px-5 flex justify-end items-center py-2 gap-2">
+                <button type="submit"
+                    class="bg-gold text-white hover:bg-white hover:ring-2 hover:ring-gold hover:text-gold px-4 py-2 w-fit rounded-[5px] font-poppins text-base">Ubah</button>
+            </div>
+        </form>
+    </div>
+</x-layout-dosen>
