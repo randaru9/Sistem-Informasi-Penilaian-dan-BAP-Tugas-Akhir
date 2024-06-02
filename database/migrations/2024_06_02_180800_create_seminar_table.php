46f4353c8f0f25859c6cb6031bf41bd53e51a33b
randaru09 <ndaruanggoro123@gmail.com>
2024-06-03 01:33:37 +0700

    feat(migrations): add seminar migration

---
+++ database/migrations/2024_06_02_180800_create_seminar_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seminar', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('bap1_id')->constrained('bap_1');
+            $table->foreignUuid('bap2_id')->constrained('bap_2');
+            $table->foreignUuid('pengguna_id')->constrained('pengguna');
+            $table->foreignUuid('pembimbing_1_id')->constrained('pengguna');
+            $table->foreignUuid('pembimbing_2_id')->constrained('pengguna');
+            $table->foreignUuid('penguji_1_id')->constrained('pengguna');
+            $table->foreignUuid('penguji_2_id')->constrained('pengguna');
+            $table->foreignUuid('pimpinan_sidang_id')->constrained('pengguna');
+            $table->string('judul');
+            $table->date('tanggal');
+            $table->time('waktu');
+            $table->timestamps();
+        });
+
+        Schema::table('penilaian', function (Blueprint $table) {
+            $table->foreignUuid('seminar_id')->constrained('seminar');
+        });
+
+        Schema::table('revisi', function (Blueprint $table) {
+            $table->foreignUuid('seminar_id')->constrained('seminar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seminar');
+    }
+};
