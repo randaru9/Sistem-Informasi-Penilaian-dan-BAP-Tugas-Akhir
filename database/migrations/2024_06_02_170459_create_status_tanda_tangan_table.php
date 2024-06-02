0bdd15d4b11d4b50735f974e0d37c7f0b8d1d7e2
randaru09 <ndaruanggoro123@gmail.com>
2024-06-03 01:31:31 +0700

    feat(migrations): add status tanda tangan migration

---
+++ database/migrations/2024_06_02_170459_create_status_tanda_tangan_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('status_tanda_tangan', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('keterangan')->unique();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('status_tanda_tangan');
+    }
+};
