a5e5f5d26b84dca3929fa3d956981075b391f8e2
randaru09 <ndaruanggoro123@gmail.com>
2024-06-03 01:32:41 +0700

    feat(migrations): add penilaian migration

---
+++ database/migrations/2024_06_02_170819_create_penilaian_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penilaian', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('pengguna_id')->constrained('pengguna');
+            $table->foreignUuid('status_penilaian_id')->constrained('status_penilaian');
+            $table->tinyInteger('penulisan_draft_tugas_akhir_dan_ppt')->nullable();
+            $table->tinyInteger('penyajian_atau_presentasi')->nullable();
+            $table->tinyInteger('penguasaan_materi')->nullable();
+            $table->tinyInteger('kemampuan_menjawab')->nullable();
+            $table->tinyInteger('etika_dan_sopan_santun')->nullable();
+            $table->tinyInteger('nilai_bimbingan')->nullable();
+            $table->string('ttd')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penilaian');
+    }
+};
