600b4093930fd0612ec90a3e51d0f099cc03301c
randaru09 <ndaruanggoro123@gmail.com>
2024-06-03 01:32:15 +0700

    feat(migrations): add pengguna migration

---
+++ database/migrations/2024_06_02_170627_create_pengguna_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengguna', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('role_id')->constrained('role');
+            $table->boolean('is_koordinator')->default(false);
+            $table->string('nama')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('nim')->unique()->nullable();
+            $table->string('nip')->unique()->nullable();
+            $table->string('password');
+            $table->string('otp');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengguna');
+    }
+};
