5c0cb39b07eea68d712df1110c7db6747a6765ee
randaru09 <ndaruanggoro123@gmail.com>
2024-06-07 06:02:15 +0700

    feat(Controllers): add GetOneRevisiById function

--- app/Http/Controllers/RevisiController.php
+++ app/Http/Controllers/RevisiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Revisi\GetAllBySeminarIdRequest;
+use App\Http\Requests\Revisi\GetOneByIdRequest;
 use App\Models\Revisi;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,12 @@ class RevisiController extends Controller
     // Get All Revisi By Seminar Id with Status
     public function GetAllRevisiBySeminarIdWithStatus(GetAllBySeminarIdRequest $request){
         $data = Revisi::where('seminar_id', $request->safe()->seminar_id)->with('StatusRevisis')->get();
+        return response()->json($data);
+    }
+
+    // Get One Revisi By Id
+    public function GetOneRevisiById(GetOneByIdRequest $request){
+        $data = Revisi::where('id', $request->safe()->id)->first();
         return response()->json($data);
     }
     
