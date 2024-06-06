472f742f48a65795aa29c3d7e5e38d2bd5bae267
randaru09 <ndaruanggoro123@gmail.com>
2024-06-06 17:23:14 +0700

    feat(Models): add relation in Pengguna Model

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -47,4 +48,8 @@ class Pengguna extends Authenticatable
     protected $casts = [
         'password' => 'hashed'
     ];
+
+    public function Roles() : BelongsTo {
+        return $this-> belongsTo(Role::class, 'role_id');
+    }
 }
