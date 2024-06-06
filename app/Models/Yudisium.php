113abf0a98366d8791279adc3b27fbd064439535
randaru09 <ndaruanggoro123@gmail.com>
2024-06-06 18:19:29 +0700

    feat(Models) : add Yudisium Model

---
+++ app/Models/Yudisium.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Yudisium extends Model
+{
+    use HasFactory, HasUuids;
+
+    use HasFactory, HasUuids;
+
+    protected $table = 'yudisium';
+
+    protected $fillable = [
+        'pengguna_id',
+        'status_yudisium_id',
+        'periode_wisuda_id',
+        'tempat_dan_bidang_kerja',
+        'saran_dan_kritik',
+        'berkas',
+        'catatan',
+    ];
+
+    public function Penggunas(): BelongsTo
+    {
+        return $this->belongsTo(Pengguna::class, 'pengguna_id');
+    }
+
+    public function StatusYudisiums(): BelongsTo
+    {
+        return $this->belongsTo(StatusYudisium::class, 'status_yudisium_id');
+    }
+
+    public function PeriodeWisudas(): BelongsTo
+    {
+        return $this->belongsTo(PeriodeWisuda::class, 'periode_wisuda_id');
+    }
+
+}
