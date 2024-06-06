f8dffae3ca4410dcd89dce4194b067e904bd0575
randaru09 <ndaruanggoro123@gmail.com>
2024-06-06 17:16:39 +0700

    feat(Models): add Pengguna Model

---
+++ app/Models/Pengguna.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Pengguna extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'is_koordinator',
+        'role_id',
+        'nama',
+        'email',
+        'nim',
+        'nip',
+        'password',
+        'otp',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'otp',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed'
+    ];
+}
