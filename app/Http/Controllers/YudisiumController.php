37d61a06e2b5dbc8f46d75eb43249a80d85de01e
randaru09 <ndaruanggoro123@gmail.com>
2024-06-07 06:20:25 +0700

    feat(Controllers): add GetYudisiumByPenggunaId function in Yudisium Controller

--- app/Http/Controllers/YudisiumController.php
+++ app/Http/Controllers/YudisiumController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Yudisium\GetByPenggunaIdRequest;
+use App\Models\Yudisium;
 use Illuminate\Http\Request;
 
 class YudisiumController extends Controller
 {
-    //
+    // (Mahasiswa) //
+
+    // Get All Yudisium by Pengguna Id
+    public function GetYudisiumByPenggunaId(GetByPenggunaIdRequest $request){
+        $data = Yudisium::where('pengguna_id', $request->safe()->pengguna_id)->get();
+        return response()->json($data);
+    }
+
+    
 }
