f74273a90aa8f3eca80a9374695d269bbcb5c2c3
randaru09 <ndaruanggoro123@gmail.com>
2024-06-07 05:07:16 +0700

    feat(Controllers): add Create Seminar function

--- app/Http/Controllers/SeminarController.php
+++ app/Http/Controllers/SeminarController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Seminar\CreateRequest;
 use App\Http\Requests\Seminar\GetByPenggunaIdRequest;
+use App\Models\Pengguna;
 use App\Models\Seminar;
 use Illuminate\Http\Request;
 
@@ -11,13 +13,22 @@ class SeminarController extends Controller
     // Get All Seminar By Pengguna Id with Count Revisi (for Status)
     public function GetSeminarByPenggunaIdWithCountRevisi(GetByPenggunaIdRequest $request){
         
-        $data = Seminar::where('pengguna_id', $request->pengguna_id)->withCount([
+        $data = Seminar::where('pengguna_id', $request->safe()->pengguna_id)->withCount([
             'Revisis' => function ($query) {
                 $query->whereHas('StatusRevisis', function ($query) {
                     $query->where('keterangan', 'Belum Selesai');
                 });
             }
         ])->get();
+
+        return response()->json($data);
+
+    }
+
+    // Create Seminar
+    public function CreateSeminar(CreateRequest $request){
+
+        $data = Seminar::create($request->safe()->all());
 
         return response()->json($data);
 
