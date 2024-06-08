ab94fd311278428e6041fdfca02072c77c5f1de8
randaru09 <ndaruanggoro123@gmail.com>
2024-06-08 20:24:31 +0700

    feat(controllers): add create yudisium function in yudisium

--- app/Http/Controllers/YudisiumController.php
+++ app/Http/Controllers/YudisiumController.php
@@ -2,19 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Yudisium\CreateRequest;
 use App\Http\Requests\Yudisium\GetByPenggunaIdRequest;
 use App\Models\Yudisium;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class YudisiumController extends Controller
 {
     // (Mahasiswa) //
 
+    // Create Yudisium
+    public function CreateYudisium(CreateRequest $request){
+        $time = Carbon::now();
+        $yudisium = $request->safe()->all();
+        $yudisium['berkas'] = $request->safe()['berkas']->store("yudisium/{$yudisium['pengguna_id']}/$time");
+        $data = Yudisium::create($yudisium);
+        return response()->json($data);
+    }    
+
     // Get All Yudisium by Pengguna Id
-    public function GetYudisiumByPenggunaId(GetByPenggunaIdRequest $request){
+    public function GetAllYudisiumByPenggunaId(GetByPenggunaIdRequest $request){
         $data = Yudisium::where('pengguna_id', $request->safe()->pengguna_id)->get();
         return response()->json($data);
     }
 
-    
+
 }
