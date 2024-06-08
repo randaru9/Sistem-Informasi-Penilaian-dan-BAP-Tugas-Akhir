0d8f264f86c4c8c6d932a20ac4a6e1b9d1b62ed2
randaru09 <ndaruanggoro123@gmail.com>
2024-06-08 20:38:31 +0700

    feat(controllers): add GetOneYudisiumById in Yudisium

--- app/Http/Controllers/YudisiumController.php
+++ app/Http/Controllers/YudisiumController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Yudisium\CreateRequest;
 use App\Http\Requests\Yudisium\GetByPenggunaIdRequest;
+use App\Http\Requests\Yudisium\GetOneByIdRequest;
 use App\Models\Yudisium;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -27,5 +28,10 @@ class YudisiumController extends Controller
         return response()->json($data);
     }
 
+    // Get One Yudisium by Id
+    public function GetOneYudisiumById(GetOneByIdRequest $request){
+        $data = Yudisium::where('id', $request->safe()->id)->first();
+        return response()->json($data);
+    }
 
 }
