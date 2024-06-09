95712564b4a1bf19b94c92271f003e78d18e9f6c
randaru09 <ndaruanggoro123@gmail.com>
2024-06-09 23:59:27 +0700

    feat(Requests): add CreateRequest in Penilaian

---
+++ app/Http/Requests/Revisi/CreateRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Revisi;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'pengguna_id' => 'required|uuid|exists:pengguna,id',
+            'seminar_id' => 'required|uuid|exists:seminar,id',
+            'status_revisi_id' => 'required|uuid|exists:status_revisi,id',
+            'keterangan' => 'required|string',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'pengguna_id' => 'Pengguna',
+            'seminar_id' => 'Seminar',
+            'status_revisi_id' => 'Status Revisi',
+            'keterangan' => 'Keterangan',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => ':attribute harus diisi',
+            'uuid' => ':attribute harus UUID',
+            'exists' => ':attribute tidak ditemukan',
+            'string' => ':attribute harus berupa string',
+        ];
+    }
+}
