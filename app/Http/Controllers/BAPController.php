d985f4b70a4f275b7af37e43802c585d1d0ed4d7
randaru09 <ndaruanggoro123@gmail.com>
2024-06-10 01:44:33 +0700

    feat(Controllers): add UpdateBAP1 function in BAP

--- app/Http/Controllers/BAPController.php
+++ app/Http/Controllers/BAPController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\BAP\CreateBAP1Request;
+use App\Http\Requests\BAP\UpdateBAP1Request;
 use App\Models\BAP1;
 use App\Models\Seminar;
 use Carbon\Carbon;
@@ -20,6 +21,15 @@ class BAPController extends Controller
         ]);
         $data = Seminar::where('id', $request->safe()->seminar_id)->update(['bap_1_id' => $bap->id]);
         return response()->json($data);
+    }
+
+    // Update BAP1 (ttd and author)
+    public function UpdateBAP1(UpdateBAP1Request $request){
+        $time = Carbon::now();
+        $bap = BAP1::where('id', $request->safe()->id)->update([
+            'ttd' => $request->safe()->ttd->store('bap/'.$request->safe()->seminar_id.'/'.$time),
+        ]);
+        return response()->json($bap);
     }
     
 
