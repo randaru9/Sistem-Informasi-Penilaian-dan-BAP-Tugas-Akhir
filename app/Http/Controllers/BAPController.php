a5b52ec0d68742a9b719322d57b363040b1b674a
randaru09 <ndaruanggoro123@gmail.com>
2024-06-10 01:30:38 +0700

    feat(Controllers): add CreateBAP1 function in BAP

---
+++ app/Http/Controllers/BAPController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BAP\CreateBAP1Request;
+use App\Models\BAP1;
+use App\Models\Seminar;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class BAPController extends Controller
+{
+    // (Dosen) //
+
+    // Create BAP1 (ttd and author)
+    public function CreateBAP1(CreateBAP1Request $request){
+        $time = Carbon::now();
+        $bap = BAP1::create([
+            'ttd' => $request->safe()->ttd->store('bap/'.$request->safe()->seminar_id.'/'.$time),
+        ]);
+        $data = Seminar::where('id', $request->safe()->seminar_id)->update(['bap_1_id' => $bap->id]);
+        return response()->json($data);
+    }
+    
+
+}
