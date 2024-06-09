8b6d42f64e0cbb7fcbaef9b4579165d1c021b56b
randaru09 <ndaruanggoro123@gmail.com>
2024-06-09 15:45:34 +0700

    feat(Controller): add CreatePenilaian function in Penilaian

---
+++ app/Http/Controllers/PenilaianController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Penilaian\CreateRequest;
+use App\Models\Penilaian;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class PenilaianController extends Controller
+{
+
+    // (Dosen) //
+
+    // Create Penilaian and Update Status Penilaian to "Selesai"
+    public function CreatePenilaian(CreateRequest $request){
+        $time = Carbon::now();
+        $data = Penilaian::create([
+            'pengguna_id' => $request->safe()->pengguna_id,
+            'seminar_id' => $request->safe()->seminar_id,
+            'status_penilaian_id' => '1', // Must change to actual id
+            'penulisan_draft_tugas_akhir_dan_ppt' => $request->safe()->penulisan_draft_tugas_akhir_dan_ppt,
+            'penyajian_atau_presentasi' => $request->safe()->penyajian_atau_presentasi,
+            'penguasaan_materi' => $request->safe()->penguasaan_materi,
+            'kemampuan_menjawab' => $request->safe()->kemampuan_menjawab,
+            'etika_dan_sopan_santun' => $request->safe()->etika_dan_sopan_santun,
+            'nilai_bimbingan' => $request->safe()->nilai_bimbingan,
+            'ttd' => $request->safe()->ttd->store('penilaian/'.$request->safe()->pengguna_id.'/'.$time),
+        ]);
+        return response()->json($data);
+    }
+}
