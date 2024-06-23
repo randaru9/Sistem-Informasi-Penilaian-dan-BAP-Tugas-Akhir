7b29ad455d0d62574c90dc667776e5fa35e6d255
randaru09 <ndaruanggoro123@gmail.com>
2024-06-23 17:56:41 +0700

    feat(controller): add UpdateKatasandiForPengguna

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Pengguna\CreateMahasiswa;
 use App\Http\Requests\Pengguna\UpdateBiodataDosen;
 use App\Http\Requests\Pengguna\UpdateBiodataMahasiswaRequest;
+use App\Http\Requests\Pengguna\UpdateKatasandiForPenggunaRequest;
 use App\Http\Requests\Pengguna\UpdateKatasandiRequest;
 use App\Models\Pengguna;
 use Illuminate\Contracts\Database\Eloquent\Builder;
@@ -15,7 +16,8 @@ class PenggunaController extends Controller
     // (Mahasiswa) //
 
     // Create Pengguna (Mahasiswa)
-    public function CreatePenggunaMahasiswa(CreateMahasiswa $request){
+    public function CreatePenggunaMahasiswa(CreateMahasiswa $request)
+    {
         // dd($request);
         Pengguna::create([
             'nama' => $request->safe()->nama,
@@ -27,7 +29,8 @@ class PenggunaController extends Controller
         return redirect()->route('mahasiswa');
     }
 
-    public function GetAllPenggunaMahasiswa(Request $request){
+    public function GetAllPenggunaMahasiswa(Request $request)
+    {
         $data = Pengguna::where(function (Builder $query) use ($request) {
             if (isset($request->search)) {
                 $query->where('nama', 'LIKE', "%{$request->search}%")
@@ -37,22 +40,25 @@ class PenggunaController extends Controller
         return view('admin.mahasiswa.mahasiswa', compact(['data']));
     }
 
-    public function GetOnePenggunaById(Request $request){
+    public function GetOnePenggunaById(Request $request)
+    {
         $data = Pengguna::findOrFail($request->id)->only(['id', 'nama', 'nim', 'email']);
         return view('admin.mahasiswa.mahasiswa-detail', compact(['data']));
     }
 
     // Update Biodata
-    public function UpdateBiodataMahasiswa(UpdateBiodataMahasiswaRequest $request){
+    public function UpdateBiodataMahasiswa(UpdateBiodataMahasiswaRequest $request)
+    {
         $data = Pengguna::where('id', $request->safe()->id)->update($request->safe()->all());
         return response()->json($data);
     }
 
     //Update Katasandi
-    public function UpdateKatasandi(UpdateKatasandiRequest $request){
+    public function UpdateKatasandi(UpdateKatasandiRequest $request)
+    {
         $katasandi_lama = Pengguna::where('id', $request->safe()->id)->value('katasandi');
-        if($katasandi_lama == $request->safe()->katasandi_lama){
-            if($request->safe()->katasandi_baru == $request->safe()->konfirmasi_katasandi_baru){
+        if ($katasandi_lama == $request->safe()->katasandi_lama) {
+            if ($request->safe()->katasandi_baru == $request->safe()->konfirmasi_katasandi_baru) {
                 $data = Pengguna::where('id', $request->safe()->id)->update([
                     'katasandi' => $request->safe()->katasandi_baru
                 ]);
@@ -66,9 +72,26 @@ class PenggunaController extends Controller
     // (Dosen) //
 
     // Update Biodata
-    public function UpdateBiodataDosen(UpdateBiodataDosen $request){
+    public function UpdateBiodataDosen(UpdateBiodataDosen $request)
+    {
         $data = Pengguna::where('id', $request->safe()->id)->update($request->safe()->all());
         return response()->json($data);
+    }
+
+    // (Admin) //
+
+    public function UpdateKatasandiForPengguna(UpdateKatasandiForPenggunaRequest $request)
+    {
+        $mahasiswa = Pengguna::where('id',$request->query('id'))->first();
+        if ($mahasiswa != null) {
+            if ($request->safe()->katasandi == $request->safe()->konfirmasi_katasandi) {
+                Pengguna::where('id', $request->query('id'))->update([
+                    'katasandi' => $request->safe()->katasandi
+                ]);
+            }
+            return back()->with('katasandi', 'Kata sandi tidak sama');
+        }
+        return back()->with('mahasiswa', 'Mahasiswa tidak ditemukan');
     }
 
 }
