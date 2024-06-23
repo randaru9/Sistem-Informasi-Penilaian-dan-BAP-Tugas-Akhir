92306562dbe59d476f0c1946ef3fea77ba2e0d33
randaru09 <ndaruanggoro123@gmail.com>
2024-06-23 17:00:45 +0700

    feat(view): add error message

--- resources/views/admin/mahasiswa/mahasiswa-buat-akun.blade.php
+++ resources/views/admin/mahasiswa/mahasiswa-buat-akun.blade.php
@@ -13,11 +13,12 @@
 
 <x-layout-admin :$breads title="Buat Akun Mahasiswa">
     <div class="bg-white ring-2 ring-blue1 rounded-[10px] w-full h-full overflow-y-auto">
-        <form action="{{route('mahasiswa-buat-akun-post')}}" method="POST">
+        <form action="{{ route('mahasiswa-buat-akun-post') }}" method="POST">
             @csrf
             <div class="w-full px-5 flex py-2 gap-2">
                 <div class="w-1/2">
-                    <p for="nama" class="block mb-2 text-xl text-[#000000] font-poppins font-bold">Buat Akun Mahasiswa</p>
+                    <p for="nama" class="block mb-2 text-xl text-[#000000] font-poppins font-bold">Buat Akun Mahasiswa
+                    </p>
                 </div>
             </div>
             <div class="w-full px-5 flex py-2 gap-2">
@@ -27,18 +28,24 @@
                     <input type="text" id="nama" name="nama"
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full "
                         minlength="9" required />
+                    @error('nama')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="w-1/2">
-                    <label for="nim"
-                        class="block mb-2 text-sm text-[#000000] font-poppins font-normal">NIM</label>
+                    <label for="nim" class="block mb-2 text-sm text-[#000000] font-poppins font-normal">NIM</label>
                     <input type="text" id="nim" name="nim"
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full "
                         minlength="9" required />
+                    @error('nim')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
             </div>
             <div class="w-full px-5 flex py-2 gap-2">
                 <div class="w-1/2">
-                    <label for="katasandi" class="block mb-2 text-sm text-[#000000] font-poppins font-normal">Katasandi</label>
+                    <label for="katasandi"
+                        class="block mb-2 text-sm text-[#000000] font-poppins font-normal">Katasandi</label>
                     <input type="password" id="katasandi" name="katasandi"
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full "
                         minlength="9" required />
