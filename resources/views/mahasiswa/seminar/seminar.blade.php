97f07658205b4174e23766328e0fba527f253013
randaru09 <ndaruanggoro123@gmail.com>
2024-06-24 19:53:15 +0700

    feat(view): page integration

--- resources/views/mahasiswa/seminar/seminar.blade.php
+++ resources/views/mahasiswa/seminar/seminar.blade.php
@@ -8,29 +8,12 @@
 
     $tablehead = ['No', 'Tanggal/Waktu Seminar', 'Status Seminar', 'Status Revisi', 'Aksi'];
 
-    $data = [
-        'array' => [
-            [
-                'Tanggal' => '10-10-2022',
-                'Waktu' => '13.00',
-                'Status Seminar' => 'Seminar Proposal',
-                'Status Revisi' => 'Belum Selesai',
-            ],
-            [
-                'Tanggal' => '10-10-2022',
-                'Waktu' => '13.00',
-                'Status Seminar' => 'Seminar Proposal',
-                'Status Revisi' => 'Belum Selesai',
-            ],
-        ],
-        'page' => 3,
-        'current' => 1,
-    ];
 @endphp
 
 <x-layout-mahasiswa :$breads title="Seminar">
     <div class="flex justify-end mb-4">
-        <a href="/mahasiswa/seminar/tambah" class="py-2 px-4 font-poppins font-medium text-white bg-gold rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold">Buat
+        <a href="/mahasiswa/seminar/tambah"
+            class="py-2 px-4 font-poppins font-medium text-white bg-gold rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold">Buat
             Jadwal Seminar</a>
     </div>
     <div>
@@ -46,20 +29,34 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($data['array'] as $item)
+                    @foreach ($data['data'] as $item)
                         <tr class="bg-white border-b font-poppins text-base font-normal">
                             <td class="px-6 py-4">
                                 {{ $loop->index + 1 }}
                             </td>
                             <td scope="row" class="px-6 py-4 font-poppins text-base font-normal">
-                                {{ $item['Tanggal'] }} / {{ $item['Waktu'] }}
+                                @php
+                                    $item['tanggal'] = date('d-m-Y', strtotime($item['tanggal']));
+                                    $item['waktu'] = date('H:i', strtotime($item['waktu']));
+                                @endphp
+                                {{ $item['tanggal'] }} / {{ $item['waktu'] }}
                             </td>
                             <td class="px-6 py-4 font-semibold font-poppins text-base">
-                                {{ $item['Status Seminar'] }}
+                                {{ $item['jenis_seminars']['keterangan'] }}
                             </td>
-                            <td class="px-6 py-4 font-poppins text-base font-normal {{$item['Status Revisi'] == 'Belum Selesai' ? 'text-red-500' : 'text-green-500'}}">
-                                {{ $item['Status Revisi'] }}
-                            </td>
+                            @if ($item['count_revisi'] === 0)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-red-500 ">
+                                    Belum Diberikan
+                                </td>
+                            @elseif($item['count_revisi_berhasil'] === 4)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500 ">
+                                    Selesai
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500 ">
+                                    Belum Selesai
+                                </td>
+                            @endif
                             <td class="px-6 py-4 font-poppins text-base font-normal">
                                 <a href="/mahasiswa/seminar/detail"
                                     class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline">Detail</a>
@@ -70,7 +67,9 @@
             </table>
         </div>
     </div>
+    @if ($data['last_page'] > 1)
     <div class="mt-4 flex justify-end">
-        <x-pagination :pages="$data['page']" :current="$data['current']" />
+        <x-pagination :pages="$data['last_page']" :current="$data['current_page']" />
     </div>
+    @endif
 </x-layout-mahasiswa>
