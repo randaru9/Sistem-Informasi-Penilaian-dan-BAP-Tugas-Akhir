9464c85cbb72c95eeb7194677aebcac76ae6d3c9
randaru09 <ndaruanggoro123@gmail.com>
2024-06-26 19:19:42 +0700

    fix(request): delete id and change some validation name

--- app/Http/Requests/Pengguna/UpdateKatasandiRequest.php
+++ app/Http/Requests/Pengguna/UpdateKatasandiRequest.php
@@ -22,27 +22,24 @@ class UpdateKatasandiRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'id' => 'uuid|exists:pengguna',   
             'katasandi_lama' => 'string|required|min_digits:8',
             'katasandi_baru' => 'string|required|min_digits:8',
-            'konfirmasi_katasandi_baru' => 'string|required|min_digits:8',
+            'konfirmasi_katasandi' => 'string|required|min_digits:8',
         ];
     }
 
     public function attributes(): array
     {
         return [
-            'id' => 'Pengguna',
             'katasandi_lama' => 'Katasandi Lama',
             'katasandi_baru' => 'Katasandi Baru',
-            'konfirmasi_katasandi_baru' => 'Konfirmasi Katasandi Baru',
+            'konfirmasi_katasandi' => 'Konfirmasi Katasandi Baru',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'uuid' => ':attribute wajib UUID',
             'string' => ':attribute harus string',
             'min_digits' => ':attribute harus lebih besar sama dengan 8 digit',
             'required' => ':attribute harus diisi',
