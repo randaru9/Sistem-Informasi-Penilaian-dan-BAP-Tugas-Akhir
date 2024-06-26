06174ddc1fbc7993004078eac487b21e3cb33a7e
randaru09 <ndaruanggoro123@gmail.com>
2024-06-26 22:45:42 +0700

    feat(view): integration only get

--- resources/views/mahasiswa/yudisium/yudisium-ubah.blade.php
+++ resources/views/mahasiswa/yudisium/yudisium-ubah.blade.php
@@ -5,11 +5,11 @@
             'title' => 'Yudisium',
         ],
         [
-            'href' => '/mahasiswa/yudisium/detail',
+            'href' => route('yudisium-detail-mahasiswa', ['id' => request()->query('id')]),
             'title' => 'Detail',
         ],
         [
-            'href' => '/mahasiswa/yudisium/ubah',
+            'href' => route('yudisium-ubah', ['id' => request()->query('id')]),
             'title' => 'Ubah',
         ],
     ];
@@ -18,14 +18,17 @@
 <x-layout-mahasiswa :$breads title="Ubah Berkas Yudisium">
     <div class="bg-white ring-2 ring-blue1 rounded-[10px] w-full h-full overflow-y-auto">
         <form action="">
+            @csrf
             <div class="w-full px-5 flex py-2 gap-2">
                 <div class="w-1/2">
                     <label for="periode_wisuda" class="block mb-2 text-sm text-[#000000] font-poppins font-normal">Periode
                         Wisuda</label>
                     <select
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full"
-                        name="periode_wisuda" id="periode_wisuda">
-                        <option value=""></option>
+                        name="periode_wisuda" id="periode_wisuda" value="{{$data['status_yudisium_id']}}">
+                        @foreach ($periode as $item)
+                            <option value="{{ $item->id }}">{{ $item->keterangan }}</option>
+                        @endforeach
                     </select>
                 </div>
             </div>
@@ -33,7 +36,7 @@
                 <div class="w-1/2">
                     <label for="tempat_kerja" class="block mb-2 text-sm text-[#000000] font-poppins font-normal">Tempat
                         dan Bidang Kerja (Apabila Sudah Bekerja)</label>
-                    <input type="text" id="tempat_kerja"
+                    <input type="text" id="tempat_kerja" value="{{ $data['tempat_dan_bidang_kerja'] }}"
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full "
                         minlength="9" required />
                 </div>
@@ -44,22 +47,20 @@
                         Kritik</label>
                     <textarea
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500"
-                        name="saran" id="saran" cols="90" rows="5"></textarea>
+                        name="saran" id="saran" cols="90" rows="5" value="{{$data['saran_dan_kritik']}}"></textarea>
                 </div>
             </div>
+            @if ($data['catatan'] != null)
             <div class="w-full px-5 flex py-2 gap-2">
                 <div class="w-4/5">
                     <label for="saran" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Catatan
                         Perbaikan :</label>
                     <p id="saran" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum at dolores beatae sed sit
-                        quisquam
-                        in quae. Facilis, ipsum. Illo velit at repellendus. Atque fugiat commodi nemo consequuntur
-                        quidem
-                        tenetur.
+                        {{ $data['catatan'] }}
                     </p>
                 </div>
             </div>
+            @endif
             <div class="w-full px-5 flex py-2 gap-2">
                 <div class="flex flex-col w-1/2">
                     <label for="berkas" class="block mb-2 text-sm text-[#000000] font-poppins font-normal">Berkas
