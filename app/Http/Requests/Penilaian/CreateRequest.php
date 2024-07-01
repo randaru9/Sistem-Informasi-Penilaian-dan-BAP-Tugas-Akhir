e58391abfd9ac3605a2ec604eed345974bed8287
randaru09 <ndaruanggoro123@gmail.com>
2024-07-01 12:49:49 +0700

    fix(request): change some validation

--- app/Http/Requests/Penilaian/CreateRequest.php
+++ app/Http/Requests/Penilaian/CreateRequest.php
@@ -22,28 +22,24 @@ class CreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'pengguna_id' => 'required|uuid|exists:pengguna',
-            'seminar_id' => 'required|uuid|exists:seminar',
-            'penulisan_draft_tugas_akhir_dan_ppt' => 'required|numeric|between:0,100',
-            'penyajian_atau_presentasi' => 'required|numeric|between:0,100',
-            'penguasaan_materi' => 'required|numeric|between:0,100',
+            'penulisan' => 'required|numeric|between:0,100',
+            'penyajian' => 'required|numeric|between:0,100',
+            'penguasaan' => 'required|numeric|between:0,100',
             'kemampuan_menjawab' => 'required|numeric|between:0,100',
-            'etika_dan_sopan_santun' => 'required|numeric|between:0,100',
-            'nilai_bimbingan' => 'numeric|between:0,100',
-            'ttd' => 'required|file|mimes:png,jpg,jpeg',
+            'etika' => 'required|numeric|between:0,100',
+            'bimbingan' => 'numeric|between:0,100',
+            'ttd' => 'required|mimes:png,jpg,jpeg|max:5240',
         ];
     }
     
     public function attributes(): array{
         return [
-            'pengguna_id' => 'Pengguna',
-            'seminar_id' => 'Seminar',
-            'penulisan_draft_tugas_akhir_dan_ppt' => 'Penulisan Draft Tugas Akhir dan PPT',
-            'penyajian_atau_presentasi' => 'Penyajian atau Presentasi',
-            'penguasaan_materi' => 'Penguasaan Materi',
+            'penulisan' => 'Penulisan Draft Tugas Akhir dan PPT',
+            'penyajian' => 'Penyajian atau Presentasi',
+            'penguasaan' => 'Penguasaan Materi',
             'kemampuan_menjawab' => 'Kemampuan Menjawab',
-            'etika_dan_sopan_santun' => 'Etika dan Sopan Santun',
-            'nilai_bimbingan' => 'Nilai Bimbingan',
+            'etika' => 'Etika dan Sopan Santun',
+            'bimbingan' => 'Nilai Bimbingan',
             'ttd' => 'Tanda Tangan',
         ];
     } 
@@ -54,7 +50,6 @@ class CreateRequest extends FormRequest
             'required' => ':attribute wajib diisi',
             'exists' => ':attribute tidak ditemukan',
             'numeric' => ':attribute harus berupa angka',
-            'file' => ':attribute harus berupa file',
             'mimes' => ':attribute harus berupa file PNG, JPG, JPEG',
             'between' => ':attribute harus antara :min sampai :max',
         ];
