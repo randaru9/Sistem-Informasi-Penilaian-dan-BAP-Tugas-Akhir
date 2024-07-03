d2b4781dabbb17abc67c7458b920602c65c68042
randaru09 <ndaruanggoro123@gmail.com>
2024-07-03 14:27:50 +0700

    feat(view): add default value for input

--- resources/views/mahasiswa/profil/profil-ubah-biodata.blade.php
+++ resources/views/mahasiswa/profil/profil-ubah-biodata.blade.php
@@ -26,7 +26,7 @@
                         class="block mb-2 text-sm text-[#000000] font-poppins font-normal">Nama</label>
                     <input type="text" id="nama" name="nama"
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full "
-                        required />
+                        required value="{{Auth::user()->nama}}"/>
                     @error('nama')
                         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                     @enderror
@@ -37,7 +37,7 @@
                     <label for="nim" class="block mb-2 text-sm text-[#000000] font-poppins font-normal">NIM</label>
                     <input type="text" id="nim" name="nim"
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full "
-                        minlength="9" required />
+                        minlength="9" required value="{{Auth::user()->nim}}" />
                     @error('nim')
                         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                     @enderror
