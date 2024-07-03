3db1b4a79f8a8ca73af5e666c3f8e6329c505c23
randaru09 <ndaruanggoro123@gmail.com>
2024-07-03 11:57:32 +0700

    feat(view): page integration

--- resources/views/dosen/profil/profil-verifikasi-email.blade.php
+++ resources/views/dosen/profil/profil-verifikasi-email.blade.php
@@ -13,7 +13,8 @@
 
 <x-layout-dosen :$breads title="Verifikasi Email">
     <div class="bg-white ring-2 ring-blue1 rounded-[10px] w-full h-full overflow-y-auto">
-        <form action="/mahasiswa/profil/verifikasi-email">
+        <form action="{{route('profil-verifikasi-email-dosen-post')}}" method="POST">
+            @csrf
             <div class="w-full px-5 flex py-2 gap-2">
                 <div class="w-1/2">
                     <p for="nama" class="block mb-2 text-xl text-[#000000] font-poppins font-bold">Verifikasi Email
@@ -24,11 +25,16 @@
                 <div class="w-1/2">
                     <label for="email" class="block mb-2 text-sm text-[#000000] font-poppins font-normal">Masukan kode
                         OTP yang dikirim ke email </label>
-                    <input type="email" id="email"
+                    <input type="text" id="otp" name="otp"
                         class="bg-white border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-1 w-full "
-                        minlength="9" required />
+                        minlength="6" required />
                 </div>
             </div>
+            @if (Session::has('otp'))
+                <div class="w-full px-5 flex py-2 gap-2">
+                    <p class="text-red-500 text-xs mt-1">{{ Session::get('otp') }}</p>
+                </div>
+            @endif
             <div class="w-full px-5 flex py-2 gap-2">
                 <div class="w-1/2 flex space-x-2">
                     <p class="text-sm text-[#000000] font-poppins font-normal">Tidak menerima kode OTP ?</p>
