39f1060f0dc2c0f7f85a3f1f23ea3271f1991446
randaru09 <ndaruanggoro123@gmail.com>
2024-07-03 14:57:34 +0700

    feat(view): page integration

--- resources/views/admin/profil/profil.blade.php
+++ resources/views/admin/profil/profil.blade.php
@@ -13,13 +13,17 @@
             <div class="w-1/2">
                 <label for="email" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Email</label>
                 <p id="email" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    ade.setiawan@staff.itera.ac.id
+                    @if ($data['email'] !== null)
+                        {{$data['email']}}
+                    @else
+                        -
+                    @endif
                 </p>
             </div>
         </div>
         <div class="w-full px-5 flex py-2 gap-2">
             <div class="w-1/2">
-                <a href="/admin/profil/ubah-email"
+                <a href="{{route('profil-ubah-email-admin')}}"
                     class="bg-gold text-white hover:bg-white hover:ring-2 hover:ring-gold hover:text-gold px-4 py-2 w-fit rounded-[5px] font-poppins text-base">
                     Ubah Email
                 </a>
@@ -29,7 +33,7 @@
             <div class="w-1/2">
                 <label for="katasandi"
                     class="block mb-2 text-base text-[#000000] font-poppins font-normal">Katasandi</label>
-                <a href="/admin/profil/ubah-katasandi"
+                <a href="{{route('profil-ubah-katasandi-admin')}}"
                     class="bg-gold text-white hover:bg-white hover:ring-2 hover:ring-gold hover:text-gold px-4 py-2 w-fit rounded-[5px] font-poppins text-base">
                     Ubah Katasandi
                 </a>
