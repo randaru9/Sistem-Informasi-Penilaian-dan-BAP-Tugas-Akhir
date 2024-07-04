5266a94103764f9964a1c1f94e3afafe8f278285
randaru09 <ndaruanggoro123@gmail.com>
2024-07-04 09:30:04 +0700

    feat(view): page integration

--- resources/views/admin/yudisium/yudisium.blade.php
+++ resources/views/admin/yudisium/yudisium.blade.php
@@ -6,38 +6,18 @@
         ],
     ];
 
-    $tablehead = ['No', 'Nama', 'Jenis Seminar', 'Status Validasi', 'Aksi'];
-
-    $data = [
-        'array' => [
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'jenis_seminar' => 'Seminar Proposal',
-                'status_validasi' => 'Diterima',
-            ],
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'jenis_seminar' => 'Seminar Proposal',
-                'status_validasi' => 'Perlu Validasi',
-            ],
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'jenis_seminar' => 'Seminar Proposal',
-                'status_validasi' => 'Ditolak',
-            ],
-        ],
-        'page' => 3,
-        'current' => 1,
-    ];
+    $tablehead = ['No', 'Nama', 'Periode Wisuda', 'Status Validasi', 'Aksi'];
 @endphp
 
 <x-layout-admin :$breads title="Yudisium">
     <div class="flex justify-start mb-4">
         <form action="" class="w-full flex space-x-4">
-            <input type="text" id="search"
+            <input type="text" id="search" name="search"
                 class="bg-white border rounded-md border-gold text-gray-900 text-sm focus:ring-gold focus:border-gold block p-1 sm:w-1/2 lg:w-3/12 font-poppins font-normal"
-                minlength="9" required placeholder="Cari nama mahasiswa" />
-            <button type="submit" class="py-2 px-4 font-poppins font-medium text-white bg-gold rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold"> Cari
+                placeholder="Cari yudisium" value="{{ request()->query('search') }}" />
+            <button type="submit"
+                class="py-2 px-4 font-poppins font-medium text-white bg-gold rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold">
+                Cari
             </button>
         </form>
     </div>
@@ -54,22 +34,32 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($data['array'] as $item)
+                    @foreach ($data['data'] as $item)
                         <tr class="bg-white border-b font-poppins text-base font-normal">
                             <td class="px-6 py-4 w-fit">
                                 {{ $loop->index + 1 }}
                             </td>
                             <td class="px-6 py-4 font-normal font-poppins text-base">
-                                {{ $item['nama'] }}
+                                {{ $item['penggunas']['nama'] }}
                             </td>
                             <td class="px-6 py-4 font-semibold font-poppins text-base">
-                                {{ $item['jenis_seminar'] }}
+                                {{ $item['periode_wisudas']['keterangan'] }}
                             </td>
-                            <td class="px-6 py-4 font-poppins text-base font-normal {{$item['status_validasi'] == 'Diterima' ? 'text-green-500' : ($item['status_validasi'] == 'Perlu Validasi' ? 'text-blue-500' : 'text-red-500')}}">
-                                {{ $item['status_validasi'] }}
-                            </td>
+                            @if ($item['status_yudisium_id'] == 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Perlu Validasi
+                                </td>
+                            @elseif ($item['status_yudisium_id'] == 2)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-red-500">
+                                    Ditolak
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Diterima
+                                </td>
+                            @endif
                             <td class="px-6 py-4 font-poppins text-base font-normal">
-                                <a href="/admin/yudisium/detail"
+                                <a href="{{ route('yudisium-detail', ['id' => $item['id']]) }}"
                                     class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline">Detail</a>
                             </td>
                         </tr>
@@ -78,7 +68,9 @@
             </table>
         </div>
     </div>
-    <div class="mt-4 flex justify-end">
-        <x-pagination :pages="$data['page']" :current="$data['current']" />
-    </div>
+    @if ($data['last_page'] > 1)
+        <div class="mt-4 flex justify-end">
+            <x-pagination :pages="$data['last_page']" :current="$data['current_page']" />
+        </div>
+    @endif
 </x-layout-admin>
