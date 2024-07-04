2dead6da96a7a3c26fc115533ca2dd0baeb7f7c3
randaru09 <ndaruanggoro123@gmail.com>
2024-07-04 09:49:44 +0700

    feat(view): page only view integration

--- resources/views/admin/yudisium/yudisium-detail.blade.php
+++ resources/views/admin/yudisium/yudisium-detail.blade.php
@@ -5,7 +5,7 @@
             'title' => 'Yudisium',
         ],
         [
-            'href' => '/admin/yudisium/detail',
+            'href' => route('yudisium-detail', ['id' => request()->query('id')]),
             'title' => 'Detail',
         ],
     ];
@@ -46,10 +46,18 @@
     <div class="bg-white ring-2 ring-blue1 rounded-[10px] w-full h-full overflow-y-auto">
         <div class="w-full px-5 flex py-2 gap-2">
             <div class="w-1/2">
+                <label for="nama" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Nama</label>
+                <p id="nama" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
+                    {{ $data['penggunas']['nama'] }}
+                </p>
+            </div>
+        </div>
+        <div class="w-full px-5 flex py-2 gap-2">
+            <div class="w-1/2">
                 <label for="periode_wisuda" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Periode
                     Wisuda</label>
                 <p id="periode_wisuda" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    November
+                    {{ $data['periode_wisudas']['keterangan'] }}
                 </p>
             </div>
         </div>
@@ -58,7 +66,7 @@
                 <label for="tempat_kerja" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Tempat
                     dan Bidang Kerja (Apabila Sudah Bekerja)</label>
                 <p id="tempat_kerja" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Staff TIK Itera
+                    {{ $data['tempat_dan_bidang_kerja'] }}
                 </p>
             </div>
         </div>
@@ -68,9 +76,7 @@
                     Saran Dan
                     Kritik</label>
                 <p id="saran" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum at dolores beatae sed sit quisquam
-                    in quae. Facilis, ipsum. Illo velit at repellendus. Atque fugiat commodi nemo consequuntur quidem
-                    tenetur.
+                    {{ $data['saran_dan_kritik'] }}
                 </p>
             </div>
         </div>
