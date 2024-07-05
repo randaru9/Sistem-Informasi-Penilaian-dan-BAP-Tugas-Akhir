e17643f7f30bced9633cface463324e4fcb30e9e
randaru09 <ndaruanggoro123@gmail.com>
2024-07-05 15:42:47 +0700

    feat(view): page integration

--- resources/views/admin/bap/bap.blade.php
+++ resources/views/admin/bap/bap.blade.php
@@ -7,43 +7,22 @@
     ];
 
     $tablehead = ['No', 'Nama', 'Jenis Seminar', 'Status Revisi', 'Status Penilaian', 'Aksi'];
-
-    $data = [
-        'array' => [
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'jenis_seminar' => 'Seminar Proposal',
-                'status_revisi' => 'Belum Diberikan',
-                'status_penilaian' => 'Belum Dinilai',
-            ],
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'jenis_seminar' => 'Seminar Proposal',
-                'status_revisi' => 'Belum Selesai',
-                'status_penilaian' => 'Sudah Dinilai',
-            ],
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'jenis_seminar' => 'Seminar Proposal',
-                'status_revisi' => 'Selesai',
-                'status_penilaian' => 'Terlambat',
-            ],
-        ],
-        'page' => 3,
-        'current' => 1,
-    ];
 @endphp
 
 <x-layout-admin :$breads title="BAP">
     <div class="flex justify-between mb-4">
         <form action="" class="w-full flex space-x-4">
-            <input type="text" id="search"
+            <input type="text" id="search" name="search"
                 class="bg-white border rounded-md border-gold text-gray-900 text-sm focus:ring-gold focus:border-gold block p-1 sm:w-1/2 lg:w-3/12 font-poppins font-normal"
-                minlength="9" required placeholder="Cari nama mahasiswa" />
-            <button type="submit" class="py-2 px-4 font-poppins font-medium text-white bg-gold rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold"> Cari
+                placeholder="Cari nama mahasiswa" value="{{ request()->query('search') }}"/>
+            <button type="submit"
+                class="py-2 px-4 font-poppins font-medium text-white bg-gold rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold">
+                Cari
             </button>
         </form>
-        <a href="/admin/bap/rekap-nilai" class="py-2 px-4 font-poppins font-medium text-white bg-gold whitespace-nowrap rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold"> Unduh Rekap Nilai
+        <a href="/admin/bap/rekap-nilai"
+            class="py-2 px-4 font-poppins font-medium text-white bg-gold whitespace-nowrap rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold">
+            Unduh Rekap Nilai
         </a>
     </div>
     <div>
@@ -59,27 +38,52 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($data['array'] as $item)
+                    @foreach ($data['data'] as $item)
                         <tr class="bg-white border-b font-poppins text-base font-normal">
                             <td class="px-6 py-4 w-fit">
                                 {{ $loop->index + 1 }}
                             </td>
                             <td class="px-6 py-4 font-normal font-poppins text-base">
-                                {{ $item['nama'] }}
+                                {{ $item['penggunas']['nama'] }}
                             </td>
                             <td class="px-6 py-4 font-semibold font-poppins text-base">
-                                {{ $item['jenis_seminar'] }}
+                                {{ $item['jenis_seminars']['keterangan'] }}
                             </td>
-                            <td
-                                class="px-6 py-4 font-poppins text-base font-normal {{ $item['status_revisi'] == 'Belum Diberikan' ? 'text-yellow-500' : ($item['status_revisi'] == 'Belum Selesai' ? 'text-red-500' : 'text-green-500') }}">
-                                {{ $item['status_revisi'] }}
-                            </td>
-                            <td
-                                class="px-6 py-4 font-poppins text-base font-normal {{ $item['status_penilaian'] == 'Belum Dinilai' ? 'text-yellow-500' : ($item['status_penilaian'] == 'Terlambat' ? 'text-red-500' : 'text-green-500') }}">
-                                {{ $item['status_penilaian'] }}
-                            </td>
+
+                            @if ($item['count_revisi'] === 0)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500 ">
+                                    Belum Diberikan
+                                </td>
+                            @elseif($item['count_revisi_selesai'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500 ">
+                                    Selesai
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500 ">
+                                    Belum Selesai
+                                </td>
+                            @endif
+
+                            @if ($item['count_penilaian'] === 0)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                    Belum Diberikan
+                                </td>
+                            @elseif($item['count_penilaian_selesai'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @elseif($item['count_penilaian_terlambat'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-red-500">
+                                    Terlambat
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @endif
+
                             <td class="px-6 py-4 font-poppins text-base font-normal">
-                                <a href="/admin/bap/detail"
+                                <a href="{{ route('bap-detail', ['id' => $item['id']]) }}"
                                     class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline">Detail</a>
                             </td>
                         </tr>
@@ -88,7 +92,10 @@
             </table>
         </div>
     </div>
+
+    @if ($data['last_page'] > 1)
     <div class="mt-4 flex justify-end">
-        <x-pagination :pages="$data['page']" :current="$data['current']" />
+        <x-pagination :pages="$data['last_page']" :current="$data['current_page']" />
     </div>
+    @endif
 </x-layout-admin>
