26d589140c815b7f8af3fb886c8fd8de1982732c
randaru09 <ndaruanggoro123@gmail.com>
2024-07-05 16:36:00 +0700

    feat(view): page integration except BAP 1, and BAP 2

--- resources/views/admin/bap/bap-detail.blade.php
+++ resources/views/admin/bap/bap-detail.blade.php
@@ -5,7 +5,7 @@
             'title' => 'BAP',
         ],
         [
-            'href' => '/admin/bap/detail',
+            'href' => route('bap-detail', ['id' => request()->query('id')]),
             'title' => 'Detail',
         ],
     ];
@@ -18,7 +18,7 @@
                 <label for="nama_teruji" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Nama
                     Teruji</label>
                 <p id="nama_teruji" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Rangga Ndaru Anggoro
+                    {{ $data['penggunas']['nama'] }}
                 </p>
             </div>
         </div>
@@ -27,8 +27,7 @@
                 <label for="judul_tugas_akhir" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Judul
                     Tugas Akhir</label>
                 <p id="judul_tugas_akhir" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    RANCANG BANGUN SISTEM INFORMASI PENILAIAN DAN BERITA ACARA PENILAIAN TUGAS AKHIR BERBASIS WEB
-                    MENGGUNAKAN METODE DSDM (Studi Kasus : Prodi Teknik Informatika ITERA)
+                    {{ $data['judul'] }}
                 </p>
             </div>
         </div>
@@ -37,14 +36,20 @@
                 <label for="tanggal_sidang" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Tanggal
                     Sidang</label>
                 <p id="tanggal_sidang" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    12 - 10 -2024
+                    @php
+                        $data['tanggal'] = date('d-m-Y', strtotime($data['tanggal']));
+                    @endphp
+                    {{ $data['tanggal'] }}
                 </p>
             </div>
             <div class="w-1/2">
                 <label for="waktu_sidang" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Waktu
                     Sidang</label>
                 <p id="waktu_sidang" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    13:00
+                    @php
+                        $data['waktu'] = date('H:i', strtotime($data['waktu']));
+                    @endphp
+                    {{ $data['waktu'] }}
                 </p>
             </div>
         </div>
@@ -53,14 +58,14 @@
                 <label for="pembimbing1" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Pembimbing
                     1</label>
                 <p id="pembimbing1" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Ilham Firman Ashari, S.Kom., M.T.
+                    {{ $data['pembimbing1s']['nama'] }}
                 </p>
             </div>
             <div class="w-1/2">
                 <label for="pembimbing2" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Pembimbing
                     2</label>
                 <p id="pembimbing2" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Andika Setiawan, S.Kom., M.Cs.
+                    {{ $data['pembimbing2s']['nama'] }}
                 </p>
             </div>
         </div>
@@ -69,14 +74,14 @@
                 <label for="penguji1" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Penguji
                     1</label>
                 <p id="penguji1" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Ir. Mugi Praseptiawan, S.T., M.Kom.
+                    {{ $data['penguji1s']['nama'] }}
                 </p>
             </div>
             <div class="w-1/2">
                 <label for="penguji2" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Penguji
                     2</label>
                 <p id="penguji2" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Miranti Verdiana, M.Si.
+                    {{ $data['penguji2s']['nama'] }}
                 </p>
             </div>
         </div>
@@ -85,31 +90,45 @@
                 <label for="pimpinan" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Pimpinan
                     Sidang</label>
                 <p id="pimpinan" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Ilham Firman Ashari, S.Kom., M.T.
+                    {{ $data['pimpinan_sidangs']['nama'] }}
                 </p>
             </div>
             <div class="w-1/2">
                 <label for="jenis_sidang" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Jenis
                     Sidang</label>
                 <p id="jenis_sidang" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Seminar Proposal
+                    {{ $data['jenis_seminars']['keterangan'] }}
                 </p>
             </div>
         </div>
         <div class="w-full px-5 flex py-2 gap-2">
             <div class="w-1/2">
+                <label for="status_revisi" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Status
+                    Revisi</label>
+                <p id="status_revisi" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
+                    @if ($data['count_revisi'] === 0)
+                        Belum Diberikan
+                    @elseif($data['count_revisi_selesai'] === 4)
+                        Selesai
+                    @else
+                        Belum Selesai
+                    @endif
+                </p>
+            </div>
+            <div class="w-1/2">
                 <label for="status_penilaian"
                     class="block mb-2 text-base text-[#000000] font-poppins font-normal">Status
                     Penilaian</label>
                 <p id="status_penilaian" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Belum Diberikan
-                </p>
-            </div>
-            <div class="w-1/2">
-                <label for="status_revisi" class="block mb-2 text-base text-[#000000] font-poppins font-normal">Status
-                    Revisi</label>
-                <p id="status_revisi" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Belum Selesai
+                    @if ($data['count_penilaian'] === 0)
+                        Belum Diberikan
+                    @elseif($data['count_penilaian_selesai'] === 4)
+                        Selesai
+                    @elseif($data['count_penilaian_terlambat'] !== 0)
+                        Terlambat
+                    @else
+                        Belum Selesai
+                    @endif
                 </p>
             </div>
         </div>
@@ -119,12 +138,16 @@
                     class="block mb-2 text-base text-[#000000] font-poppins font-normal">Status
                     Tanda Tangan</label>
                 <p id="status_penilaian" class="text-sm text-[#000000] font-poppins font-normal w-2/3 text-justify">
-                    Belum Diberikan
+                    @if ($data['Bap1s_count'] === 0)
+                        Belum Diberikan
+                    @else
+                        Sudah Diberikan
+                    @endif
                 </p>
             </div>
         </div>
         <div class="w-full px-5 flex justify-end items-center py-2 gap-2">
-            <a href="/admin/bap/detail-proses"
+            <a href="{{route('bap-detail-proses', ['id' => request()->query('id')])}}"
                 class="bg-gold text-white hover:bg-white hover:ring-2 hover:ring-gold hover:text-gold px-4 py-2 w-fit rounded-[5px] font-poppins text-base">Detail
                 Proses</a>
             <a href="/admin/bap/lihat-bap1"
