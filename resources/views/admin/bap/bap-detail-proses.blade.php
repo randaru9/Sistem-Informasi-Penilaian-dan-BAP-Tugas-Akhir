03006a9948c94ed8e2cca4af687ed1990046246a
randaru09 <ndaruanggoro123@gmail.com>
2024-07-08 10:22:21 +0700

    feat(view): page integration

--- resources/views/admin/bap/bap-detail-proses.blade.php
+++ resources/views/admin/bap/bap-detail-proses.blade.php
@@ -5,47 +5,16 @@
             'title' => 'BAP',
         ],
         [
-            'href' => '/admin/bap/detail',
+            'href' => route('bap-detail', ['id' => request()->query('id')]),
             'title' => 'Detail',
         ],
         [
-            'href' => '/admin/bap/detail-proses',
+            'href' => route('bap-detail-proses', ['id' => request()->query('id')]),
             'title' => 'Detail Proses',
         ],
     ];
 
     $tablehead = ['No', 'Dosen', 'Posisi Dosen', 'Status Revisi', 'Status Penilaian', 'Aksi'];
-
-    $data = [
-        'array' => [
-            [
-                'nama' => 'Ilham Firman Ashari, S.Kom., M.T.',
-                'posisi_dosen' => 'Pembimbing 1',
-                'status_revisi' => 'Belum Diberikan',
-                'status_penilaian' => 'Belum Dinilai',
-            ],
-            [
-                'nama' => 'Andika Setiawan, S.Kom., M.Cs.',
-                'posisi_dosen' => 'Pembimbing 2',
-                'status_revisi' => 'Belum Selesai',
-                'status_penilaian' => 'Terlambat',
-            ],
-            [
-                'nama' => 'Ir. Mugi Praseptiawan, S.T., M.Kom.',
-                'posisi_dosen' => 'Penguji 1',
-                'status_revisi' => 'Selesai',
-                'status_penilaian' => 'Belum Dinilai',
-            ],
-            [
-                'nama' => 'Miranti Verdiana, M.Si.',
-                'posisi_dosen' => 'Penguji 2',
-                'status_revisi' => 'Belum Diberikan',
-                'status_penilaian' => 'Selesai',
-            ],
-        ],
-        'page' => 3,
-        'current' => 1,
-    ];
 @endphp
 
 <x-layout-admin :$breads title="Detail Proses BAP">
@@ -62,36 +31,252 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($data['array'] as $item)
-                        <tr class="bg-white border-b font-poppins text-base font-normal">
-                            <td class="px-6 py-4 w-fit">
-                                {{ $loop->index + 1 }}
+
+                    {{-- // Pembimbing 1 --}}
+
+                    <tr class="bg-white border-b font-poppins text-base font-normal">
+                        <td class="px-6 py-4 w-fit">
+                            1
+                        </td>
+                        <td class="px-6 py-4 font-normal font-poppins text-base">
+                            {{ $collection['pembimbing1s']['nama'] }}
+                        </td>
+                        <td class="px-6 py-4 font-semibold font-poppins text-base">
+                            Pembimbing 1
+                        </td>
+                        @if ($collection['revisi_pembimbing_1'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                Belum Diberikan
                             </td>
-                            <td class="px-6 py-4 font-normal font-poppins text-base">
-                                {{ $item['nama'] }}
+                        @else
+                            @if ($collection['revisi_pembimbing_1']['status_revisi_id'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @endif
+                        @endif
+                        @if ($collection['penilaian_pembimbing_1'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                Belum Diberikan
                             </td>
-                            <td class="px-6 py-4 font-semibold font-poppins text-base">
-                                {{ $item['posisi_dosen'] }}
-                            </td>
-                            <td
-                                class="px-6 py-4 font-poppins text-base font-normal {{ $item['status_revisi'] == 'Belum Diberikan' ? 'text-yellow-500' : ($item['status_revisi'] == 'Belum Selesai' ? 'text-red-500' : 'text-green-500') }}">
-                                {{ $item['status_revisi'] }}
-                            </td>
-                            <td
-                                class="px-6 py-4 font-poppins text-base font-normal {{ $item['status_penilaian'] == 'Belum Dinilai' ? 'text-yellow-500' : ($item['status_penilaian'] == 'Terlambat' ? 'text-red-500' : 'text-green-500') }}">
-                                {{ $item['status_penilaian'] }}
-                            </td>
+                        @else
+                            @if ($collection['penilaian_pembimbing_1']['status_penilaian_id'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @elseif ($collection['penilaian_pembimbing_1']['status_penilaian_id'] === 2)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-red-500">
+                                    Terlambat
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @endif
+                        @endif
+                        @if ($collection['penilaian_pembimbing_1'] === [])
                             <td class="px-6 py-4 font-poppins text-base font-normal">
-                                <a href="/admin/bap/form-penilaian"
-                                    class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline whitespace-nowrap">Lihat Form</a>
+                                <p
+                                    class="font-medium text-gray-500 whitespace-nowrap">
+                                    Penilaian Belum Ada</p>
                             </td>
-                        </tr>
-                    @endforeach
+                        @else
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <a href="{{ route('bap-form-penilaian', ['id' => $collection['penilaian_pembimbing_1']['id']]) }}"
+                                    class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline whitespace-nowrap">Lihat
+                                    Penilaian</a>
+                            </td>
+                        @endif
+                    </tr>
+
+                    {{-- // Pembimbing 2 --}}
+
+                    <tr class="bg-white border-b font-poppins text-base font-normal">
+                        <td class="px-6 py-4 w-fit">
+                            2
+                        </td>
+                        <td class="px-6 py-4 font-normal font-poppins text-base">
+                            {{ $collection['pembimbing2s']['nama'] }}
+                        </td>
+                        <td class="px-6 py-4 font-semibold font-poppins text-base">
+                            Pembimbing 2
+                        </td>
+                        @if ($collection['revisi_pembimbing_2'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                Belum Diberikan
+                            </td>
+                        @else
+                            @if ($collection['revisi_pembimbing_2']['status_revisi_id'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @endif
+                        @endif
+                        @if ($collection['penilaian_pembimbing_2'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                Belum Diberikan
+                            </td>
+                        @else
+                            @if ($collection['penilaian_pembimbing_2']['status_penilaian_id'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @elseif ($collection['penilaian_pembimbing_2']['status_penilaian_id'] === 2)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-red-500">
+                                    Terlambat
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @endif
+                        @endif
+                        @if ($collection['penilaian_pembimbing_2'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <p
+                                    class="font-medium text-gray-500 whitespace-nowrap">
+                                    Penilaian Belum Ada</p>
+                            </td>
+                        @else
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <a href="{{ route('bap-form-penilaian', ['id' => $collection['penilaian_pembimbing_2']['id']]) }}"
+                                    class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline whitespace-nowrap">Lihat
+                                    Penilaian</a>
+                            </td>
+                        @endif
+                    </tr>
+
+                    {{-- // Penguji 1 --}}
+
+                    <tr class="bg-white border-b font-poppins text-base font-normal">
+                        <td class="px-6 py-4 w-fit">
+                            3
+                        </td>
+                        <td class="px-6 py-4 font-normal font-poppins text-base">
+                            {{ $collection['penguji1s']['nama'] }}
+                        </td>
+                        <td class="px-6 py-4 font-semibold font-poppins text-base">
+                            Penguji 1
+                        </td>
+                        @if ($collection['revisi_penguji_1'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                Belum Diberikan
+                            </td>
+                        @else
+                            @if ($collection['revisi_penguji_1']['status_revisi_id'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @endif
+                        @endif
+                        @if ($collection['penilaian_penguji_1'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                Belum Diberikan
+                            </td>
+                        @else
+                            @if ($collection['penilaian_penguji_1']['status_penilaian_id'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @elseif ($collection['penilaian_penguji_1']['status_penilaian_id'] === 2)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-red-500">
+                                    Terlambat
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @endif
+                        @endif
+                        @if ($collection['penilaian_penguji_1'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <p
+                                    class="font-medium text-gray-500 whitespace-nowrap">
+                                    Penilaian Belum Ada</p>
+                            </td>
+                        @else
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <a href="{{ route('bap-form-penilaian', ['id' => $collection['penilaian_penguji_1']['id']]) }}"
+                                    class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline whitespace-nowrap">Lihat
+                                    Penilaian</a>
+                            </td>
+                        @endif
+                    </tr>
+
+                    {{-- Penguji 2 --}}
+
+                    <tr class="bg-white border-b font-poppins text-base font-normal">
+                        <td class="px-6 py-4 w-fit">
+                            4
+                        </td>
+                        <td class="px-6 py-4 font-normal font-poppins text-base">
+                            {{ $collection['penguji2s']['nama'] }}
+                        </td>
+                        <td class="px-6 py-4 font-semibold font-poppins text-base">
+                            Penguji 2
+                        </td>
+                        @if ($collection['revisi_penguji_2'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                Belum Diberikan
+                            </td>
+                        @else
+                            @if ($collection['revisi_penguji_2']['status_revisi_id'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @endif
+                        @endif
+                        @if ($collection['penilaian_penguji_2'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal text-blue-500">
+                                Belum Diberikan
+                            </td>
+                        @else
+                            @if ($collection['penilaian_penguji_2']['status_penilaian_id'] === 1)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Selesai
+                                </td>
+                            @elseif ($collection['penilaian_penguji_2']['status_penilaian_id'] === 2)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-red-500">
+                                    Terlambat
+                                </td>
+                            @else
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-yellow-500">
+                                    Belum Selesai
+                                </td>
+                            @endif
+                        @endif
+                        @if ($collection['penilaian_penguji_2'] === [])
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <p
+                                    class="font-medium text-gray-500 whitespace-nowrap">
+                                    Penilaian Belum Ada</p>
+                            </td>
+                        @else
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <a href="{{ route('bap-form-penilaian', ['id' => $collection['penilaian_penguji_2']['id']]) }}"
+                                    class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline whitespace-nowrap">Lihat
+                                    Penilaian</a>
+                            </td>
+                        @endif
+                    </tr>
                 </tbody>
             </table>
         </div>
-    </div>
-    <div class="mt-4 flex justify-end">
-        <x-pagination :pages="$data['page']" :current="$data['current']" />
     </div>
 </x-layout-admin>
