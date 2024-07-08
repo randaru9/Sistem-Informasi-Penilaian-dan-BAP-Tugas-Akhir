ba85ad40be26f022ef36656a7103fb64cb70de94
randaru09 <ndaruanggoro123@gmail.com>
2024-07-08 11:33:17 +0700

    feat(view): page integration

--- resources/views/admin/bap/bap-form-penilaian.blade.php
+++ resources/views/admin/bap/bap-form-penilaian.blade.php
@@ -5,15 +5,15 @@
             'title' => 'BAP',
         ],
         [
-            'href' => '/admin/bap/detail',
+            'href' => route('bap-detail', ['id' => request()->query('id')]),
             'title' => 'Detail',
         ],
         [
-            'href' => '/admin/bap/detail-proses',
+            'href' => route('bap-detail-proses', ['id' => $data['seminars']['id']]),
             'title' => 'Detail Proses',
         ],
         [
-            'href' => '/admin/bap/form-penilaian',
+            'href' => route('bap-form-penilaian', ['id' => request()->query('id')]),
             'title' => 'Lihat Form Penilaian',
         ],
     ];
@@ -30,9 +30,15 @@
                             <p class="my-2">
                                 FORM 05
                             </p>
-                            <p class="mb-2">
-                                FORM PENILAIAN SIDANG (PROPOSAL/AKHIR)
-                            </p>
+                            @if ($data['seminars']['jenis_seminar_id'] == 1)
+                                <p class="mb-2">
+                                    FORM PENILAIAN SIDANG (PROPOSAL/<s>AKHIR</s>)
+                                </p>
+                            @else
+                                <p class="mb-2">
+                                    FORM PENILAIAN SIDANG (<s>PROPOSAL</s>/AKHIR)
+                                </p>
+                            @endif
                             <p class="mb-2">
                                 PROGRAM STUDI TEKNIK INFORMATIKA
                             </p>
@@ -47,25 +53,23 @@
                             <div class="font-tmr font-normal text-base flex space-x-1">
                                 <p class="whitespace-nowrap w-32">Nama</p>
                                 <p> : </p>
-                                <p>Rangga Ndaru Anggoro</p>
+                                <p>{{ $data['seminars']['penggunas']['nama'] }}</p>
                             </div>
                             <div class="font-tmr font-normal text-base flex space-x-1">
                                 <p class="whitespace-nowrap w-32">NIM</p>
                                 <p> : </p>
-                                <p>120140019</p>
+                                <p>{{ $data['seminars']['penggunas']['nim'] }}</p>
                             </div>
                             <div class="font-tmr font-medium text-base flex space-x-1">
                                 <p class="whitespace-nowrap w-32">Judul Tugas Akhir</p>
                                 <p>:</p>
-                                <p class="w-10/12">RANCANG BANGUN SISTEM INFORMASI PENILAIAN DAN BERITA
-                                    ACARA PENILAIAN TUGAS AKHIR BERBASIS WEB MENGGUNAKAN METODE
-                                    DSDM (Studi Kasus : Prodi Teknik Informatika ITERA)</p>
+                                <p class="w-10/12">{{ $data['seminars']['judul'] }}</p>
                             </div>
                         </div>
                         <div class=" mt-2">
                             <table class="border border-black w-full font-tmr text-base">
                                 <thead>
-                                    <tr >
+                                    <tr>
                                         <th class="border border-black p-2">No</th>
                                         <th class="border border-black p-2">Poin Penilaian</th>
                                         <th class="border border-black p-2">Nilai Angka</th>
@@ -74,43 +78,82 @@
                                 <tbody>
                                     <tr>
                                         <td class="border border-black p-2 text-center font-tmr text-base">1</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base">Penulisan Draft Tugas Akhir dan PPT
+                                        <td class="border border-black p-2 text-center font-tmr text-base">Penulisan
+                                            Draft Tugas Akhir dan PPT
                                         </td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base"></td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            {{ $data['penulisan_draft_tugas_akhir_dan_ppt'] }}
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td class="border border-black p-2 text-center font-tmr text-base">2</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base">Penyajian/Presentasi</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base"></td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            Penyajian/Presentasi</td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            {{ $data['penyajian_atau_presentasi'] }}
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td class="border border-black p-2 text-center font-tmr text-base">3</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base">Penguasaan Materi
+                                        <td class="border border-black p-2 text-center font-tmr text-base">Penguasaan
+                                            Materi
                                         </td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base"></td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            {{ $data['penguasaan_materi'] }}
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td class="border border-black p-2 text-center font-tmr text-base">4</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base">Kemampuan Menjawab</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base"></td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">Kemampuan
+                                            Menjawab</td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            {{ $data['kemampuan_menjawab'] }}
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td class="border border-black p-2 text-center font-tmr text-base">5</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base">Etika dan Sopan Santuan</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base"></td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">Etika dan
+                                            Sopan Santuan</td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            {{ $data['etika_dan_sopan_santun'] }}
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td class="border border-black p-2 text-center font-tmr text-base">6</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base">Nilai Bimbingan</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base"></td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">Nilai
+                                            Bimbingan</td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            {{ $data['nilai_bimbingan'] }}
+                                        </td>
                                     </tr>
                                     <tr>
-                                        <td class="border border-black p-2 text-center font-tmr text-base font-semibold" colspan="2">Total</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base"></td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base font-semibold"
+                                            colspan="2">Total</td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            @php
+                                                $total =
+                                                    $data['penulisan_draft_tugas_akhir_dan_ppt'] +
+                                                    $data['penyajian_atau_presentasi'] +
+                                                    $data['penguasaan_materi'] +
+                                                    $data['kemampuan_menjawab'] +
+                                                    $data['etika_dan_sopan_santun'] +
+                                                    $data['nilai_bimbingan'];
+                                            @endphp
+                                            {{ $total }}
+                                        </td>
                                     </tr>
                                     <tr>
-                                        <td class="border border-black p-2 text-center font-tmr text-base font-semibold" colspan="2">Rata-rata</td>
-                                        <td class="border border-black p-2 text-center font-tmr text-base"></td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base font-semibold"
+                                            colspan="2">Rata-rata</td>
+                                        <td class="border border-black p-2 text-center font-tmr text-base">
+                                            @php
+                                                $average = $total / 6;
+                                                if ($data['nilai_bimbingan'] === 0) {
+                                                    $average = $total / 5;
+                                                }
+                                            @endphp
+                                            {{ $average }}
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -118,14 +161,24 @@
                         <div class="mt-4 space-y-1 flex justify-end">
                             <div class="w-fit">
                                 <div class="font-tmr text-left text-base">
-                                    <p class="font-normal">Lampung Selatan, 2 April 2024</p>
-                                    <p class="font-bold">Ketua Sidang Proposal,</p>
+                                    @php
+                                        $data['created_at'] = date('d-m-Y', strtotime($data['created_at']));
+                                    @endphp
+                                    <p class="font-normal">Lampung Selatan, {{ $data['created_at'] }}</p>
+                                    @if ($data['pengguna_id'] == $data['seminars']['pembimbing_1_id'])
+                                        <p class="font-bold">Pembimbing 1</p>
+                                    @elseif($data['pengguna_id'] == $data['seminars']['pembimbing_2_id'])
+                                        <p class="font-bold">Pembimbing 2</p>
+                                    @elseif($data['pengguna_id'] == $data['seminars']['penguji_1_id'])
+                                        <p class="font-bold">Penguji 1</p>
+                                    @elseif($data['pengguna_id'] == $data['seminars']['penguji_2_id'])
+                                        <p class="font-bold">Penguji 2</p>
+                                    @endif
                                     <div class="w-[100px] h-[100px]">
-
                                     </div>
-                                    <p class="font-bold"> Ilham Firman Ashari, S.Kom., M.T.
+                                    <p class="font-bold"> {{ $data['penggunas']['nama'] }}
                                     </p>
-                                    <p class="font-normal"> NIP. 19930314 201903 1 018
+                                    <p class="font-normal"> NIP. {{ $data['penggunas']['nip'] }}
                                     </p>
                                 </div>
                             </div>
