e5103d21e27e52fc9a81124a53aedf1fd1b38b68
randaru09 <ndaruanggoro123@gmail.com>
2024-07-08 15:09:12 +0700

    feat(view): page integration

--- resources/views/dosen/bap/bap.blade.php
+++ resources/views/dosen/bap/bap.blade.php
@@ -7,43 +7,17 @@
     ];
 
     $tablehead = ['No', 'Nama', 'Jenis Seminar', 'Tanggal/Waktu', 'Status Tanda Tangan', 'Aksi'];
-
-    $data = [
-        'array' => [
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'jenis_seminar' => 'Seminar Proposal',
-                'tanggal' => '12-10-2024',
-                'waktu' => '13:00',
-                'status_tanda_tangan' => 'Belum Di Tandatangani',
-            ],
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'tanggal' => '12-10-2024',
-                'waktu' => '13:00',
-                'jenis_seminar' => 'Seminar Proposal',
-                'status_tanda_tangan' => 'Belum Di Tandatangani',
-            ],
-            [
-                'nama' => 'Rangga Ndaru Anggoro',
-                'tanggal' => '12-10-2024',
-                'waktu' => '13:00',
-                'jenis_seminar' => 'Seminar Proposal',
-                'status_tanda_tangan' => 'Sudah Di Tandatangani',
-            ],
-        ],
-        'page' => 3,
-        'current' => 1,
-    ];
 @endphp
 
 <x-layout-dosen :$breads title="BAP">
     <div class="flex justify-start mb-4">
         <form action="" class="w-full flex space-x-4">
-            <input type="text" id="search"
+            <input type="text" id="search" name="search"
                 class="bg-white border rounded-md border-gold text-gray-900 text-sm focus:ring-gold focus:border-gold block p-1 sm:w-1/2 lg:w-3/12 font-poppins font-normal"
-                minlength="9" required placeholder="Cari nama mahasiswa" />
-            <button type="submit" class="py-2 px-4 font-poppins font-medium text-white bg-gold rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold"> Cari
+                placeholder="Cari nama mahasiswa" value="{{ request()->query('search') }}" />
+            <button type="submit"
+                class="py-2 px-4 font-poppins font-medium text-white bg-gold rounded-[4px] hover:bg-white hover:text-gold hover:ring-2 hover:ring-gold">
+                Cari
             </button>
         </form>
     </div>
@@ -60,35 +34,37 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($data['array'] as $item)
+                    @foreach ($data['data'] as $item)
                         <tr class="bg-white border-b font-poppins text-base font-normal">
                             <td class="px-6 py-4 w-fit">
                                 {{ $loop->index + 1 }}
                             </td>
                             <td class="px-6 py-4 font-normal font-poppins text-base">
-                                {{ $item['nama'] }}
+                                {{ $item['penggunas']['nama'] }}
                             </td>
                             <td class="px-6 py-4 font-semibold font-poppins text-base">
-                                {{ $item['jenis_seminar'] }}
+                                {{ $item['jenis_seminars']['keterangan'] }}
                             </td>
                             <td scope="row" class="px-6 py-4 font-poppins text-base font-normal">
+                                @php
+                                    $item['tanggal'] = date('d-m-Y', strtotime($item['tanggal']));
+                                    $item['waktu'] = date('H:i', strtotime($item['waktu']));
+                                @endphp
                                 {{ $item['tanggal'] }} / {{ $item['waktu'] }}
                             </td>
-                            <td
-                                class="px-6 py-4 font-poppins text-base font-normal {{ $item['status_tanda_tangan'] == 'Belum Di Tandatangani' ? 'text-red-500' : 'text-green-500' }}">
-                                {{ $item['status_tanda_tangan'] }}
-                            </td>
-                            @if ($item['status_tanda_tangan'] == 'Sudah Di Tandatangani')
-                                <td class="px-6 py-4 font-poppins text-base font-normal">
-                                    <a href="/dosen/penilaian/detail"
-                                        class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline">Detail</a>
+                            @if ($item['b_a_p1s']['ttd'] === null)
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-red-500">
+                                    Belum Di Tandatangani
                                 </td>
                             @else
-                                <td class="px-6 py-4 font-poppins text-base font-normal">
-                                    <a href="/dosen/bap/tambah-tanda-tangan"
-                                        class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline">Beri Tanda Tangan</a>
+                                <td class="px-6 py-4 font-poppins text-base font-normal text-green-500">
+                                    Sudah Di Tandatangani
                                 </td>
                             @endif
+                            <td class="px-6 py-4 font-poppins text-base font-normal">
+                                <a href="{{route('bap-tambah-tanda-tangan', ['id' => $item['id']])}}"
+                                    class="font-medium text-blue1 hover:text-[#0F548D] dark:text-blue-500 underline">Detail</a>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
@@ -96,6 +72,6 @@
         </div>
     </div>
     <div class="mt-4 flex justify-end">
-        <x-pagination :pages="$data['page']" :current="$data['current']" />
+        <x-pagination :pages="$data['last_page']" :current="$data['current_page']" />
     </div>
 </x-layout-dosen>
