6ad86ae4c7dbb11d5e530fd1b7f0935e4a17b2db
randaru09 <ndaruanggoro123@gmail.com>
2024-07-09 14:18:39 +0700

    feat(component): name and role integration

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,8 +6,16 @@
                 <img src="{{ url(asset('storage/assets/logo_notif.svg')) }}" alt="logo_notif">
             </button>
             <div>
-                <p class="font-poppins font-semibold text-base">Rangga</p>
-                <p class="font-poppins font-normal text-xs">Mahasiswa</p>
+                <p class="font-poppins font-semibold text-base">
+                    {{ Auth::user()->nama }}
+                </p>
+                @if (Auth::user()->role_id == 1)
+                    <p class="font-poppins font-normal text-xs">Admin</p>
+                @elseif(Auth::user()->role_id == 2)
+                    <p class="font-poppins font-normal text-xs">Dosen</p>
+                @elseif(Auth::user()->role_id == 3)
+                    <p class="font-poppins font-normal text-xs">Mahasiswa</p>
+                @endif
             </div>
             <img src="{{ url(asset('storage/assets/logo_profil.svg')) }}" alt="logo_profil">
         </div>
