597d23f267d0877e4330a2386051a73e70fd10f5
randaru09 <ndaruanggoro123@gmail.com>
2024-07-09 12:49:25 +0700

    feat(controller): add AddTTDBAP1

--- app/Http/Controllers/BAPController.php
+++ app/Http/Controllers/BAPController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BAP\AddTTDBAP1Request;
 use App\Http\Requests\BAP\CreateBAP1Request;
 use App\Http\Requests\BAP\UpdateBAP1Request;
 use App\Http\Requests\Seminar\GetByIdRequest;
@@ -13,7 +14,17 @@ use Illuminate\Http\Request;
 class BAPController extends Controller
 {
     // (Dosen) //
-
-    
+    public function AddTTDBAP1(AddTTDBAP1Request $request){
+        if ($request->query('id')){
+            $data = BAP1::where('id', $request->query('id'))->first();
+            $ttd = $request->safe()->ttd->store("/ttd/bap/{$data->id}");
+            $data->update([
+                'status_tanda_tangan_id' => 2,
+                'ttd' => $ttd
+            ]);
+            return redirect()->route('bap-dosen');
+        }
+        return redirect()->route('bap-dosen');
+    }
 
 }
