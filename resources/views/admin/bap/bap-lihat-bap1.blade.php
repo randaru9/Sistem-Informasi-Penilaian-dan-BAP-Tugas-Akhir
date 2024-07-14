2de647f13058d804ec26d173d2c8b0f0880ec0ea
randaru09 <ndaruanggoro123@gmail.com>
2024-07-14 19:09:19 +0700

    feat(view): add route in href

--- resources/views/admin/bap/bap-lihat-bap1.blade.php
+++ resources/views/admin/bap/bap-lihat-bap1.blade.php
@@ -288,7 +288,7 @@
                 </div>
             </div>
             <div class="flex justify-center mt-4">
-                <a href="/admin/bap/unduh-bap1"
+                <a href="{{route('bap-unduh-bap1', ['id' => request()->query('id')])}}"
                     class="bg-gold text-white hover:bg-white hover:ring-2 hover:ring-gold hover:text-gold px-4 py-1 w-fit rounded-[5px] font-poppins text-base">
                     Unduh
                 </a>
