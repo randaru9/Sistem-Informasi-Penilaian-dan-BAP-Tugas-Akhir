937388bdfe3efe0452656ef1c8fe238c70724851
randaru09 <ndaruanggoro123@gmail.com>
2024-08-07 12:58:39 +0700

    feat(view): add mail content

---
+++ resources/views/mail/otp.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <p>Halo, {{ $data['nama'] }}</p>
+    <p>Kami telah menerima permintaan Anda untuk merubah katasandi akun Anda. Untuk melanjutkan proses ini, silakan gunakan kode OTP berikut: </p>
+
+    <p>{{ $data['otp'] }}</p>
+
+    <p>Jika Anda tidak merasa meminta perubahan katasandi ini, harap abaikan email ini dan pastikan keamanan akun Anda.</p>
+    <p>Terima Kasih</p>
+
+    <p>Sistem Informasi Penilaian dan BAP Tugas Akhir</p>
+    <p>Program Studi Teknik Informatika</p>
+    <p>Institut Teknologi Sumatera</p>
+</div>
