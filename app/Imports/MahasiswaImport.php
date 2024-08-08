80aa5a97ae109bf21a18102c68e5305b8315880a
randaru09 <ndaruanggoro123@gmail.com>
2024-08-08 11:22:21 +0700

    feat(import): add condition withTrashed

--- app/Imports/MahasiswaImport.php
+++ app/Imports/MahasiswaImport.php
@@ -9,13 +9,24 @@ use Maatwebsite\Excel\Concerns\ToCollection;
 class MahasiswaImport implements ToCollection
 {
     /**
-    * @param Collection $collection
-    */
+     * @param Collection $collection
+     */
     public function collection(Collection $rows)
     {
-        foreach($rows as $row) {
-            if($row->filter()->isNotEmpty()){
-                Pengguna::create([
+        foreach ($rows as $row) {
+            if ($row->filter()->isNotEmpty()) {
+                $mahasiswa = Pengguna::withTrashed()->where('nim', $row[1])->orWhere('nama', $row[0])->first();
+                if ($mahasiswa == null) {
+                    Pengguna::create([
+                        'nama' => $row[0],
+                        'nim' => $row[1],
+                        'password' => $row[1],
+                        'role_id' => 3,
+                    ]);
+                    return redirect()->route('mahasiswa');
+                }
+                $mahasiswa->restore();
+                $mahasiswa->update([
                     'nama' => $row[0],
                     'nim' => $row[1],
                     'password' => $row[1],
