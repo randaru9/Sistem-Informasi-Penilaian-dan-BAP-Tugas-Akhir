e5399c413bd841a745cd5e079e09f92390b924ec
randaru09 <ndaruanggoro123@gmail.com>
2024-08-27 23:48:37 +0700

    feat: add pengingat mail message

---
+++ resources/views/mail/pengingat.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <p>Halo, {{ $data['nama'] }}</p>
+    
+    <p>Sepertinya Anda memiliki penilaian tugas akhir yang melewati batas waktu penilaian. Silahkan buka sistem penilaian dan BAP tugas akhir, lalu selesaikan penilaian Anda secepatnya.</p>
+    <p>Terima Kasih</p>
+
+    <p>Sistem Informasi Penilaian dan BAP Tugas Akhir</p>
+    <p>Program Studi Teknik Informatika</p>
+    <p>Institut Teknologi Sumatera</p>
+</div>
